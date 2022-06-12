43b2771bcd0f8562bedd19816da15c26fac4ea72
Curious Aditya <83276781+believeinaditya12@users.noreply.github.com>
2022-06-12 21:27:37 +0530

    app file done

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+let age = 32;
+let userName = 'Max'
+let hobbies = ['Sports', 'Cooking', 'Reading']
+let job = { title: 'Developer', place: 'New York', salary: 50000 };
+let totaladultyears
+function calculateadultyears(userage) {
+ return userage-18
+}
+totaladultyears = calculateadultyears(age)
+alert(totaladultyears)
+age = 45
+totaladultyears = calculateadultyears(age)
+alert(totaladultyears)
+alert(hobbies);
+function 
+// use alert(hobbies[0]) if we want to use only one string in the alert section
+alert(job.title)
+let adultyears = age - 18
+alert(adultyears)
